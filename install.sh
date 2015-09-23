a8fe6878190ba46fca3a913e08eb5aec41132477
Miroslav Safr <Miroslav.Safr@gmail.com>
2015-09-23 20:35:49 +0300

    appver-installer 1.3.0
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

--- install.sh
+++ install.sh
@@ -1,46 +1,20 @@
 #!/bin/sh
-#Rapid VM builder - http://safrm.net/projects/rvmb
+#Rapid Virtual Machine Builder
+#web: http://safrm.net/projects/rvmb
 #author: Miroslav Safr <miroslav.safr@gmail.com>
-BINDIR=/usr/bin
-COMPLETION_DIR=/etc/bash_completion.d
 SYSCONFDIR=/etc
-MANDIR=/usr/share/man
+. appver-installer
 
-#root check
-USERID=`id -u`
-[ $USERID -eq "0" ] || {
-    echo "I cannot continue, you should be root or run it with sudo!"
-    exit 0
-}
+appver_basic_scripts_test
 
-#automatic version
-if command -v appver 1>/dev/null 2>&1; then . appver; else APP_SHORT_VERSION=NA ; APP_FULL_VERSION_TAG=NA ; APP_BUILD_DATE=`date +'%Y%m%d_%H%M'`; fi
-#test
-for TEST in $(  grep -r -l -h --exclude-dir=.git --exclude-dir=test "#\!/bin/sh" . )
-do
-    sh -n $TEST
-    if  [ $? != 0 ]; then
-        echo "syntax error in $TEST, exiting.."
-        exit 1
-    fi
-done
-
-#update documentation
-jss-docs-update ./doc
-
-mkdir -p -m 0755 $BINDIR
-install -m 0777 -v ./rvmb  $BINDIR/
-sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/rvmb && rm -f $BINDIR/rvmb.bkp
-sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/rvmb && rm -f $BINDIR/rvmb.bkp
-
+$MKDIR_755 $BINDIR
+$INSTALL_755 ./rvmb  $BINDIR
+appver_update_version_and_date $BINDIR/rvmb
 
 TARGETS=`find ./targets -type f`
-mkdir -p -m 0755 $SYSCONFDIR/rvmb/targets
-install -m 755 $TARGETS $SYSCONFDIR/rvmb/targets
+$MKDIR_755 $SYSCONFDIR/rvmb/targets
+$INSTALL_755 $TARGETS $SYSCONFDIR/rvmb/targets
 
-mkdir -p -m 0755 $COMPLETION_DIR
-install -m 0777 -v ./rvmb_completion  $COMPLETION_DIR/
-
-mkdir -p -m 0644 $MANDIR/man1
-install -m 644 ./doc/manpages/rvmb.1 $MANDIR/man1
+$MKDIR_755 $COMPLETION_DIR
+$INSTALL_755 ./rvmb_completion  $COMPLETION_DIR/
 
